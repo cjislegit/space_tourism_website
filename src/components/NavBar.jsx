8df88408de2fe92ca1e480acb44f37458cbb1abb
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-04-17 20:39:49 -0700

    Added hamburger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,19 @@
 import logo from '../assets/shared/logo.svg';
+import hamburger from '../assets/shared/icon-hamburger.svg';
 
 const NavBar = () => {
   return (
-    <nav className='flex bg-indigo-500 h-20 w-full'>
+    <nav className='inline-flex bg-indigo-500 h-20 w-full items-center justify-between px-[24px]'>
       <div>
         <img src={logo} alt='' className='h-[40px] w-[40px] ' />
       </div>
-      <div></div>
+      <div>
+        <img
+          src={hamburger}
+          alt=''
+          className='h-[24px] w-[21px] justify-self-end'
+        />
+      </div>
     </nav>
   );
 };
