67fb0531e8b520c0ac92776f2d44f2511ac6e321
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-04-17 20:16:06 -0700

    Added logo to nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,14 @@
+import logo from '../assets/shared/logo.svg';
+
 const NavBar = () => {
-  return <nav></nav>;
+  return (
+    <nav className='flex bg-indigo-500 h-20 w-full'>
+      <div>
+        <img src={logo} alt='' className='h-[40px] w-[40px] ' />
+      </div>
+      <div></div>
+    </nav>
+  );
 };
 
 export default NavBar;
