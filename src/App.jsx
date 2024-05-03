dfd4cf1fd8173e14c14f6c02c003bc090c81cafe
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-05-02 20:44:57 -0700

    Added react router and set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './App.css';
 import NavBar from './components/NavBar';
+import RootLayout from './pages/Root';
+import HomePage from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [{ index: true, element: <HomePage /> }],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <NavBar />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
