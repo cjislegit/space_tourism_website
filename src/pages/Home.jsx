183418caa9d56661273eeb078ae3e849d3fae6cb
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-05-02 21:35:11 -0700

    Started styling the fist section of home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
 const HomePage = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <section className='text-white pt-[60px] px-6 text-center'>
+        <p className='barlow-regular text-base'>SO, YOU WANT TO TRAVEL TO</p>
+        <h1 className='bellefair-regular text-[80px]'>SPACE</h1>
+        <p className='barlow-regular text-base'>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </section>
+    </>
+  );
 };
 
 export default HomePage;
