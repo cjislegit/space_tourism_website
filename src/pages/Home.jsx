5e782fb4badb2e48654b36da0af391eae9eda7d4
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-05-08 20:13:36 -0700

    Started styling the tablet layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 const HomePage = () => {
   return (
     <>
-      <section className='text-white pt-[60px] px-6 text-center h-1/2'>
-        <p className='barlow-regular text-base'>SO, YOU WANT TO TRAVEL TO</p>
-        <h1 className='bellefair-regular text-[80px]'>SPACE</h1>
-        <p className='barlow-regular text-base'>
+      <section className='text-white pt-[60px] px-6 text-center h-1/2 flex flex-col  items-center'>
+        <p className='barlow-regular text-base md:text-[20px]'>
+          SO, YOU WANT TO TRAVEL TO
+        </p>
+        <h1 className='bellefair-regular text-[80px] md:text-[150px]'>SPACE</h1>
+        <p className='barlow-regular text-base max-w-[444px]'>
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
@@ -12,7 +14,7 @@ const HomePage = () => {
         </p>
       </section>
       <section className=' flex items-center justify-center flex-grow'>
-        <div className='bg-white bellefair-regular text-[20px] flex justify-center items-center w-[150px] h-[150px] rounded-full tex'>
+        <div className='bg-white bellefair-regular text-[20px] flex justify-center items-center w-[150px] h-[150px] rounded-full md:w-[242px] md:h-[242px] md:text-[32px]'>
           <a href=''>EXPLORE</a>
         </div>
       </section>
