865ed4de8f7a1f3a28ca7f3633b00fcb959d0f2d
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-05-08 21:06:45 -0700

    Started styling the tablet nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,29 @@ import hamburger from '../assets/shared/icon-hamburger.svg';
 
 const NavBar = () => {
   return (
-    <nav className='inline-flex  h-20 w-full items-center justify-between px-[24px]'>
+    <nav className='inline-flex  h-20 w-full items-center justify-between px-[24px] md:pr-0'>
       <div>
         <img src={logo} alt='' className='h-[40px] w-[40px] ' />
       </div>
-      <div>
+      <div className='md:w-1/2 md:h-full '>
+        <nav className='text-white text-[14px] uppercase h-full flex justify-evenly bg-white bg-opacity-[4%] backdrop-blur-md'>
+          <a className='md:h-full flex items-center' href=''>
+            Home
+          </a>
+          <a className='md:h-full flex items-center' href=''>
+            Destination
+          </a>
+          <a className='md:h-full flex items-center' href=''>
+            Crew
+          </a>
+          <a className='md:h-full flex items-center' href=''>
+            technology
+          </a>
+        </nav>
         <img
           src={hamburger}
           alt=''
-          className='h-[24px] w-[21px] justify-self-end'
+          className='h-[24px] w-[21px] justify-self-end md:hidden'
         />
       </div>
     </nav>
