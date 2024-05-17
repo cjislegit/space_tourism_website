c0846bb4a388d07186ffa101d58c46b341b276d6
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-05-16 20:46:33 -0700

    Styled and built logic for the first half of the destination page

---
+++ src/assets/destination/DestinationImgs.jsx
@@ -0,0 +1,29 @@
+import moon from './image-moon.webp';
+import europa from './image-europa.webp';
+import mars from './image-mars.webp';
+import titan from './image-titan.webp';
+
+const DestinationImgs = {
+  moon: {
+    name: 'Moon',
+    image: moon,
+    alt: 'Photo of the Moon',
+  },
+  europa: {
+    name: 'Europa',
+    image: europa,
+    alt: 'Photo of the Europa',
+  },
+  mars: {
+    name: 'Mars',
+    image: mars,
+    alt: 'Photo of the Mars',
+  },
+  titan: {
+    name: 'Titan',
+    image: titan,
+    alt: 'Photo of the Titan',
+  },
+};
+
+export default DestinationImgs;
--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,14 +1,91 @@
+import { useState } from 'react';
+import DestinationImgs from '../assets/destination/DestinationImgs';
+
 const DestinationPage = () => {
+  const { moon, mars, europa, titan } = DestinationImgs;
+
+  const [destinationChoice, setDestinationChoice] = useState('moon');
+
+  const handleChoiceClick = (choice) => {
+    setDestinationChoice(choice);
+  };
+
+  let destination;
+
+  switch (destinationChoice) {
+    case 'moon':
+      destination = moon;
+      break;
+    case 'mars':
+      destination = mars;
+      break;
+    case 'europa':
+      destination = europa;
+      break;
+    case 'titan':
+      destination = titan;
+      break;
+    default:
+      destination = moon;
+      break;
+  }
+
   return (
     <>
-      <section className='text-white barlow-condensed-regular flex flex-col items-center'>
+      <section className='text-white barlow-condensed-regular flex flex-col items-center text-[16px] uppercase tracking-[2.7px]'>
         <p>
-          <span className='opacity-[25%] font-bold pr-[18px]'>01</span> Pick
+          <span className='opacity-[25%] font-bold pr-[18px] '>01</span> Pick
           Your Destination
         </p>
       </section>
-      <section>
-        <img src='' alt='' />
+      <section className='flex flex-col items-center mt-[32px]'>
+        <img
+          src={destination.image}
+          alt={moon.alt}
+          className='w-[170px] h-[170px]'
+        />
+        <div className='text-white flex w-full justify-between pr-[70px] pl-[70px] barlow-condensed-regular text-[14px] tracking-[2.3px] mt-[26px]'>
+          <div
+            className={
+              destinationChoice == 'moon'
+                ? 'border-b-2 border-white pb-[2px]'
+                : null
+            }
+            onClick={() => handleChoiceClick('moon')}
+          >
+            MOON
+          </div>
+          <div
+            className={
+              destinationChoice == 'mars'
+                ? 'border-b-2 border-white pb-[2px]'
+                : null
+            }
+            onClick={() => handleChoiceClick('mars')}
+          >
+            MARS
+          </div>
+          <div
+            className={
+              destinationChoice == 'europa'
+                ? 'border-b-2 border-white pb-[2px]'
+                : null
+            }
+            onClick={() => handleChoiceClick('europa')}
+          >
+            EUROPA
+          </div>
+          <div
+            className={
+              destinationChoice == 'titan'
+                ? 'border-b-2 border-white pb-[2px]'
+                : null
+            }
+            onClick={() => handleChoiceClick('titan')}
+          >
+            TITAN
+          </div>
+        </div>
       </section>
     </>
   );
