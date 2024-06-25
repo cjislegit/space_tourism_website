00a0011cd441a979e23d584e41b4511d9c143626
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-06-24 20:06:57 -0700

    Styled the photo in the Crew page and started styling the circular divs

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -10,14 +10,24 @@ const CrewPage = () => {
     <>
       <section className='text-white barlow-condensed-regular flex flex-col items-center text-[16px] uppercase tracking-[2.7px] md:pt-[40px] 2xl:pl-[117px] 2xl:self-center 2xl:flex-grow'>
         <p className='md:self-start md:pl-[24px] md:text-[20px] 2xl:text-[28px] 2xl:mb-[97px] 2xl:tracking-widest'>
-          <span className='opacity-[25%] font-bold pr-[18px] '>01</span> Meet
+          <span className='opacity-[25%] font-bold pr-[18px] '>02</span> Meet
           Your Crew
         </p>
-        <img
-          src={crew[crewMember].images.webp}
-          // alt={moon.alt}
-          className='w-[170px] h-[170px] mt-[32px] md:w-[300px] md:h-[300px] md:mt-[60px] 2xl:w-[445px] 2xl:h-[445px] 2xl:mt-0 2xl:self-start 2xl:ml-[85px]'
-        />
+        <div className='w-[90%] flex justify-center border-b-[1px] border-[#383B4B]'>
+          <img
+            src={crew[crewMember].images.webp}
+            // alt={moon.alt}
+            className='w-[177px] h-[222px] mt-[32px] md:w-[300px] md:h-[300px] md:mt-[60px] 2xl:w-[445px] 2xl:h-[445px] 2xl:mt-0 2xl:self-start 2xl:ml-[85px]'
+          />
+        </div>
+      </section>
+      <section>
+        <div>
+          <div className='w-[10px] h-[10px] rounded-full bg-white'></div>
+          <div className='w-[10px] h-[10px] rounded-full bg-white'></div>
+          <div className='w-[10px] h-[10px] rounded-full bg-white'></div>
+          <div className='w-[10px] h-[10px] rounded-full bg-white'></div>
+        </div>
       </section>
     </>
   );
