14700f50a2aac7b5f8e8ac717bf541ab8bc214da
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-06-27 20:30:46 -0700

    Started styling the crew page for tablets

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { DATA } from '../data';
 
 const CrewPage = () => {
-  const [crewMember, setCrewMember] = useState(3);
+  const [crewMember, setCrewMember] = useState(0);
 
   const crew = DATA.crew;
 
@@ -17,7 +17,7 @@ const CrewPage = () => {
           <span className='opacity-[25%] font-bold pr-[18px] '>02</span> Meet
           Your Crew
         </p>
-        <div className='w-[90%] flex justify-center border-b-[1px] border-[#383B4B]'>
+        <div className='w-[90%] flex justify-center border-b-[1px] border-[#383B4B] md:hidden'>
           <img
             src={crew[crewMember].images.webp}
             // alt={moon.alt}
@@ -26,7 +26,7 @@ const CrewPage = () => {
         </div>
       </section>
       <section className=' flex flex-wrap mt-[32px] justify-center text-center'>
-        <div className='flex w-[33%] justify-around'>
+        <div className='flex w-[33%] justify-around md:hidden'>
           <div
             className={`w-[10px] h-[10px] rounded-full ${
               crewMember == 0 ? 'bg-white' : 'bg-[#383B4B]'
@@ -53,15 +53,41 @@ const CrewPage = () => {
           ></div>
         </div>
         <div className='text-white mt-[32px] w-[90%]'>
-          <div className='bellefair-regular  text-[16px] uppercase'>
+          <div className='bellefair-regular  text-[16px] uppercase md:text-[24px]'>
             {crew[crewMember].role}
           </div>
-          <div className='bellefair-regular uppercase text-[24px]'>
+          <div className='bellefair-regular uppercase text-[24px] md:text-[40px]'>
             {crew[crewMember].name}
           </div>
-          <div className='barlow-regular text-[15px] px-[15px] text-center mt-[16px] leading-relaxed'>
+          <div className='barlow-regular text-[15px] px-[15px] text-center mt-[16px] leading-relaxed md:px-[20%]'>
             {crew[crewMember].bio}
           </div>
+        </div>
+        <div className='flex w-[33%] justify-around mt-[40px] max-w-[88px]'>
+          <div
+            className={`w-[10px] h-[10px] rounded-full ${
+              crewMember == 0 ? 'bg-white' : 'bg-[#383B4B]'
+            }`}
+            onClick={() => handleCrewClick(0)}
+          ></div>
+          <div
+            className={`w-[10px] h-[10px] rounded-full ${
+              crewMember == 1 ? 'bg-white' : 'bg-[#383B4B]'
+            }`}
+            onClick={() => handleCrewClick(1)}
+          ></div>
+          <div
+            className={`w-[10px] h-[10px] rounded-full ${
+              crewMember == 2 ? 'bg-white' : 'bg-[#383B4B]'
+            }`}
+            onClick={() => handleCrewClick(2)}
+          ></div>
+          <div
+            className={`w-[10px] h-[10px] rounded-full ${
+              crewMember == 3 ? 'bg-white' : 'bg-[#383B4B]'
+            }`}
+            onClick={() => handleCrewClick(3)}
+          ></div>
         </div>
       </section>
     </>
